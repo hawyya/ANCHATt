a6e08f6ebca1f5288b7a4354916c89828b4db536
vladimirlbkn <119615319+hawyya@users.noreply.github.com>
2024-03-27 19:16:06 +0300

    Update main.js

--- main.js
+++ main.js
@@ -27,45 +27,29 @@ let join = `
 console.log(hello)
 console.log(join)
 
-// const readln = require('readline')
-// const rl = readln.createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-// });
-
-// rl.question("press a button and then hit enter: ", (answer => {
-//     if(answer ==+ '1'){
-//         console.log("u pressed 1")
-//     }else if(anwser ==+ '2'){
-//         console.log("u pressed 2")
-        
-//     }else if(anwser === '3'){
-//         console.log("u pressed 3")
-//     }else{
-//         console.log("no such anwser")
-//     }
-//     rl.close()
-// }),)
-
-process.stdin.setEncoding('utf8');
-
-process.stdout.write("press a button and then hit enter: ");
-
-process.stdin.on('data', (input) => {
-    const answer = input.trim();
-
-    if (answer === '1') {
-        console.log("You pressed 1");
-    } else if (answer === '2') {
-        console.log("You pressed 2");
-    } else if (answer === '3') {
-        console.log("You pressed 3");
-    } else {
-        console.log("No such answer");
-    }
-
-    process.exit();
+const readln = require('readline')
+const rl = readln.createInterface({
+    input: process.stdin,
+    output: process.stdout
 });
+
+rl.question("press a button and then hit enter: ", (answer => {
+    if(answer ==+ '1'){
+        console.log("u pressed 1")
+    }else if(anwser ==+ '2'){
+        console.log("u pressed 2")
+        
+    }else if(anwser === '3'){
+        console.log("u pressed 3")
+    }else{
+        console.log("no such anwser")
+    }
+    rl.close()
+}),)
+
+
+
+
 
 
 
